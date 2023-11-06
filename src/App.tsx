e9c7d0b9046790b3a9e251c681d36ab7bc8103c6
NahomiVRojas <nahomivillanueva2@gmail.com>
2023-11-06 10:57:42 -0500

    Nuevo proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center mt-10 flex-col">
+        <p className="font-bold text-blue-600 text-7xl">Hola Equipo Frontend</p>
+        <p className="font-bold text-red-600 text-7xl">Comencemos a codear!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,tsx,ts,js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
