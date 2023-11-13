eacb9d1032dfb2ef280465e3dd74061daa6cfc1c
Miguel Paricahua Lopez <mig.parlop@gmail.com>
2023-11-13 12:38:53 -0500

    layout de la app actualizada

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div className="h-screen bg-white absolute top-0 left-0">
+      <h1>Navbar</h1>
+      <h1>Navbar</h1>
+
+      <h1>Navbar</h1>
+
+      <h1>Navbar</h1>
+
+      <h1>Navbar</h1>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 import { Outlet } from "react-router-dom";
 
+import Navbar from "@/components/navbar";
+
 function AppLayout() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const btnUpdateMenuVisibility = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="bg-[#283C4C] w-full h-full">
-      <h2>APPP LATYOUT</h2>
-      <Outlet />
+    <div className="w-full h-screen flex flex-col">
+      <div className="bg-primary h-[4rem] relative">
+        <div className="h-full flex justify-between items-center">
+          <button onClick={btnUpdateMenuVisibility}>MENU</button>
+          <div>PERFIL</div>
+        </div>
+        {menuOpen && <Navbar />}
+      </div>
+      <div className="bg-blue-300 grow">
+        <Outlet />
+        <h1>asdfasdf</h1>
+      </div>
     </div>
   );
 }
