fcc43414a6ddb7d4bfdaea3a171eb4f68dcddc32
Cesar Brandon <cesarbrandon000@gmail.com>
2023-11-21 09:59:34 -0500

    :bug: Auth schema

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-  username: z.string().min(8, { message: "Campo requerido" }),
-  password: z.string().min(8, { message: "Campo requerido" }),
+  username: z.string().min(1, { message: "Campo requerido" }),
+  password: z.string().min(1, { message: "Campo requerido" }),
   remember: z.boolean().default(false).optional(),
 });
 
