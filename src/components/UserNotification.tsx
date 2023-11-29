9990d7f9af0fa42f1a7ae14041cfb68ab5c44276
walmerHerrer <0512018020@alumnos.unp.edu.pe>
2023-11-29 13:45:40 -0500

    mejoras menu notificaciones

--- src/components/UserNotification.tsx
+++ src/components/UserNotification.tsx
@@ -5,36 +5,45 @@ import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent } from "@/components/ui/card";
 import { Archive, ArchiveX, ArchiveRestore } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator } from "./ui/dropdown-menu";
 
 interface Notification {
   id: number;
+  title: string;
   notification: string;
 }
 
 const list: Notification[] = [
   {
     id: 0,
-    notification: "test0",
+    title: "Titulo 0",
+    notification: "Esta es una prueba para ver el estado del texto en caso que ocupe todo el ancho",
   },
   {
     id: 1,
+    title: "Titulo 1",
     notification: "test1",
   },
   {
     id: 2,
+    title: "Titulo 2",
     notification: "test2",
   },
   {
     id: 3,
+    title: "Titulo 3",
     notification: "test3",
   },
   {
     id: 4,
+    title: "Titulo 4",
     notification: "test4",
   },
   {
     id: 5,
-    notification: "test5",
+    title: "Titulo 5",
+    notification: "Esta es una prueba para ver el estado del texto en caso que ocupe todo el ancho",
   },
 ];
 
@@ -80,19 +89,21 @@ export default function UserNotification() {
   };
 
   return (
-    <div className="notification-container relative">
-      {listNotification.length > 0 && (
-        <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-destructive" />
-      )}
-      <div
-        className=" p-[.4rem] rounded-full bg-primary cursor-pointer"
-        onClick={handleNotification}
-      >
-        <BellIcon className="text-black" />
-      </div>
-      {notificationOpen && (
-        <div className="absolute top-[42px] right-0 bg-popover">
-          <ScrollArea className="h-[200px] w-60 rounded-md border">
+    <>
+      <DropdownMenu >
+        <DropdownMenuTrigger>
+          <div
+            className="relative p-[.4rem] rounded-full bg-primary cursor-pointer"
+            onClick={handleNotification}
+          >
+            {listNotification.length > 0 && (
+              <span className="absolute right-0 top-0 mt-0 h-3 w-3 rounded-full bg-destructive" ></span>
+            )}
+            <BellIcon className="text-black" />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-80">
+          <ScrollArea className="h-[220px] rounded-md border">
             <div className="p-3">
               <Tabs defaultValue="notificaciones" className="w-full">
                 <TabsList className="grid w-full grid-cols-2">
@@ -112,7 +123,12 @@ export default function UserNotification() {
                             <div key={listNot.id}>
                               <div className="flex justify-between items-center">
                                 <div className="text-sm">
-                                  {listNot.notification}
+                                  <h1 className="text-lg">
+                                    {listNot.title}
+                                  </h1>
+                                  <h3 >
+                                    {listNot.notification}
+                                  </h3>
                                 </div>
                                 <button
                                   onClick={() => handleArchive(listNot.id)}
@@ -137,7 +153,8 @@ export default function UserNotification() {
                           <div key={archive.id}>
                             <div className="flex justify-between items-center">
                               <div className="text-sm">
-                                {archive.notification}
+                                <h1 className="text-lg">{archive.title}</h1>
+                                <h3 className="text text-sm">{archive.notification}</h3>
                               </div>
                               <div className="flex gap-3">
                                 <button>
@@ -168,8 +185,104 @@ export default function UserNotification() {
               </Tabs>
             </div>
           </ScrollArea>
-        </div>
-      )}
-    </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {/*<DropdownMenu>
+          <div className="notification-container relative">
+            {listNotification.length > 0 && (
+              <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-destructive" />
+            )}
+            <div
+              className=" p-[.4rem] rounded-full bg-primary cursor-pointer"
+              onClick={handleNotification}
+            >
+              <BellIcon className="text-black" />
+            </div>
+            {notificationOpen && (
+              <div className="absolute top-[42px] right-0 bg-popover">
+                <ScrollArea className="h-[220px] w-[320px] rounded-md border">
+                  <div className="p-3">
+                    <Tabs defaultValue="notificaciones" className="w-full">
+                      <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="notificaciones">
+                          Notificaciones
+                        </TabsTrigger>
+                        <TabsTrigger value="archivados">Archivados</TabsTrigger>
+                      </TabsList>
+                      <TabsContent value="notificaciones">
+                        {listNotification.length > 0 ? (
+                          <Card className="border-none">
+                            <CardContent className="space-y-2 px-4">
+                              {listNotification
+                                .slice()
+                                .reverse()
+                                .map((listNot) => (
+                                  <div key={listNot.id}>
+                                    <div className="flex justify-between items-center">
+                                      <div className="text-sm">
+                                        {listNot.notification}
+                                      </div>
+                                      <button
+                                        onClick={() => handleArchive(listNot.id)}
+                                      >
+                                        <Archive />
+                                      </button>
+                                    </div>
+                                    <Separator className="my-2" />
+                                  </div>
+                                ))}
+                            </CardContent>
+                          </Card>
+                        ) : (
+                          <span className="text-sm px-4">No hay notificaciones</span>
+                        )}
+                      </TabsContent>
+                      <TabsContent value="archivados">
+                        {archives.length > 0 ? (
+                          <Card className="border-none">
+                            <CardContent className="space-y-2 px-4">
+                              {archives.map((archive) => (
+                                <div key={archive.id}>
+                                  <div className="flex justify-between items-center">
+                                    <div className="text-sm">
+                                      {archive.notification}
+                                    </div>
+                                    <div className="flex gap-3">
+                                      <button>
+                                        <ArchiveRestore
+                                          onClick={() =>
+                                            restoreListNotification(archive.id)
+                                          }
+                                        />
+                                      </button>
+                                      <button>
+                                        <ArchiveX
+                                          onClick={() =>
+                                            deleteListArchives(archive.id)
+                                          }
+                                        />
+                                      </button>
+                                    </div>
+                                  </div>
+                                  <Separator className="my-2" />
+                                </div>
+                              ))}
+                            </CardContent>
+                          </Card>
+                        ) : (
+                          <span className="text-sm  px-4">No hay archivados</span>
+                        )}
+                      </TabsContent>
+                    </Tabs>
+                  </div>
+                </ScrollArea>
+              </div>
+            )}
+          </div>
+                        </DropdownMenu>*/}
+      
+      
+    </>
+    
   );
 }
