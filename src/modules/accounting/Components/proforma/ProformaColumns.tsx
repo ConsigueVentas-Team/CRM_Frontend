9fa367ef8aec35654fea5f413633404b61c4f46f
Cesar Brandon <cesarbrandon000@gmail.com>
2023-12-05 12:42:09 -0500

    :sparkles: Pagina Proforma (close #28)

---
+++ src/modules/accounting/Components/proforma/ProformaColumns.tsx
@@ -0,0 +1,123 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, Download, Eye } from "lucide-react";
+
+export const proformaColumns: ColumnDef<Proforma>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        className="ml-4"
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "numero_proforma",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nº Proforma
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="lowercase">{row.getValue("numero_proforma")}</div>
+    ),
+  },
+  {
+    accessorKey: "referencia",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Referencia
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="lowercase">{row.getValue("referencia")}</div>
+    ),
+  },
+  {
+    accessorKey: "fecha",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Fecha
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("fecha")}</div>,
+  },
+  {
+    accessorKey: "total",
+    header: ({ column }) => {
+      return (
+        <div className="text-right">
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Total
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      );
+    },
+
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("total"));
+
+      const formatted = new Intl.NumberFormat("es-PE", {
+        style: "currency",
+        currency: "PEN",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const item = row.original;
+      return (
+        <div className="text-right">
+          <Button variant="ghost" size="icon">
+            <Download className="h-4 w-4" />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <Eye className="h-4 w-4" />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
--- src/modules/user/pages/Users.tsx
+++ src/modules/user/pages/Users.tsx
@@ -7,11 +7,22 @@ import { UserDetail } from "@/types/auth";
 export function Users() {
   useTitle("Usuarios");
 
-  const [users, setUsers] = useState<UserDetail[]>([]);
+  const [users, setUsers] = useState<UserDetail[]>([
+    {
+      id: 1,
+      username: "username",
+      email: "email",
+      nombre: "nombre",
+      apellidos: "apellidos",
+      position_name: "position_name",
+      core_name: "core_name",
+      department_name: "department_name",
+    }
+  ]);
 
   return (
     <section className="py-6 flex flex-col gap-8">
-      <h3 className="text-2xl">Usuarios</h3>
+      <h3 className="text-3xl">Usuarios</h3>
       <div className="flex gap-4">
         <UserActions setUsers={setUsers}/>
       </div>
