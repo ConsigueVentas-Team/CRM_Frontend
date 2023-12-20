a8bba974076b1af00523721442849f6486e5f1c6
Cesar Brandon <cesarbrandon000@gmail.com>
2023-12-20 09:13:51 -0500

    :sparkles: Proforma create finalized (close #29)

--- src/modules/accounting/Components/proforma/ProformaForm.tsx
+++ src/modules/accounting/Components/proforma/ProformaForm.tsx
@@ -7,45 +7,49 @@ import { ProformaFormPersonnel } from "./ProformaFormPersonnel";
 import { ProformaFormObservation } from "./ProformaFormObservations";
 import { ProformaFormPackages } from "./ProformaFormPackages";
 import { ProformaFormBasicInfo } from "./ProformaFormBasicInfo";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { packageIntermediate } from "./management/data";
+import { useAuth } from "@/hooks/useAuth";
 interface Props {
   onSubmit: (data: z.infer<typeof ProformaScheme>) => void;
 }
 
+// NOTE: cambiar requered_by por required_by
+
 export function ProformaForm({ onSubmit }: Props) {
+  const { user } = useAuth();
+  const [packages, setPackages] = useState<Package[]>(packageIntermediate);
   const form = useForm<z.infer<typeof ProformaScheme>>({
     resolver: zodResolver(ProformaScheme),
     defaultValues: {
       invoice_number: "001",
       date: new Date().toLocaleDateString(),
       reference: "",
-      prepared_by: "",
-      required_by: "",
+      prepared_by: user?.nombre + " " + user?.apellidos,
+      requered_by: "",
       approved_by: "Jhoel Fernández A.",
       email: "",
       phone_number: "",
       work_time: "",
-      company: 0,
-      type: "Basica",
+      company_id: 0,
+      type: "Intermedia",
       observations: [],
-      package: [],
+      package: packages,
       personal_proyecto: [],
     },
   });
 
-  function viewValues() {
-    console.log(form.getValues());
-  }
+  useEffect(() => {
+    form.setValue("package", packages);
+  }, [packages]);
 
   return (
     <Form {...form}>
-      <Button variant="outline" className="w-40" onClick={viewValues}>
-preview de los valores
-      </Button>
+      <Button type="submit" onClick={() => {console.log(form.getValues())}}>datos</Button>
       <form id="add-proforma-form" onSubmit={form.handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-6">
-          <ProformaFormBasicInfo form={form} />
+          <ProformaFormBasicInfo form={form} setPackages={setPackages} />
           <ProformaFormPackages form={form} />
           <ProformaFormPersonnel form={form} />
           <ProformaFormObservation form={form} />
