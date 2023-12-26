e991db45012b34d13e5bdbd1a95aa7a0da395580
moisesvi <moisesvargasms@gmail.com>
2023-12-26 10:25:42 -0500

    GET da datos de las areas e items y parpadep (close #37)

--- src/modules/accounting/Components/PreviewPDF.tsx
+++ src/modules/accounting/Components/PreviewPDF.tsx
@@ -1,17 +1,44 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import { PDF } from "../pages/PDF";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
+import { toast } from "@/hooks/useToast";
 
-const queryClient = new QueryClient();
 interface Props {
   dataDetail: ProformaDataTable;
 }
 const PreviewPDF = ({ dataDetail }: Props) => {
+  const [data, setData] = useState<ProformaPDF>()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fechData = async () => {
+    setIsLoading(true)
+
+    try {
+      api.get(`/proformas/${dataDetail.proforma_id}`).then((res) => {
+        console.log(res.data);
+        setData(res.data)
+      })
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "No se pudo obtener la proforma",
+        variant: "destructive",
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fechData()
+  }, [])
+
   return (
     <PDFViewer style={{ width: "100%", height: "80vh" }}>
-      <QueryClientProvider client={queryClient}>
-        <PDF dataDetail={dataDetail} />
-      </QueryClientProvider>
+      {
+        data && <PDF data={data} />
+      }
     </PDFViewer>
   );
 };
