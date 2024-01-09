0f20059f81ddda25cb39c6034e248c6054549fb0
German <German_francis@hotmail.com>
2024-01-09 10:55:40 -0500

    corrección información cliente

--- src/modules/client/components/ClientDetail.tsx
+++ src/modules/client/components/ClientDetail.tsx
@@ -13,14 +13,22 @@ interface Props {
   client: ClientDetailType;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function ClientDetail({ client }: Props) {
   return (
     <SheetContent>
       <SheetTitle>Información del cliente</SheetTitle>
       <div className="pt-8">
         <div className="flex flex-col items-center gap-4">
-          <CLientDataEditable/>
+          <img
+            src="https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBlcmZpbCUyMGRlJTIwaG9tYnJlfGVufDB8fDB8fHww"
+            alt="perfil"
+            className="rounded-full w-48 h-48 2xl:w-60 2xl:h-60 flex-initial object-cover"
+          />
+          <p className="flex flex-col items-center mb-[0.5rem]">
+            {client.nombre} {client.apellidos}
+            <span className="text-muted-foreground"></span>
+          </p>
+          <CLientDataEditable />
         </div>
       </div>
       <SheetFooter className="mt-8 md:mt-3 sm:justify-center gap-9">
