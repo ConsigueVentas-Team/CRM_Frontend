c2ce6a2a703e46ee3a205e84cc83b4ba1116fd0d
Cesar Brandon <cesarbrandon000@gmail.com>
2024-03-13 12:38:55 -0500

    :lipstick: List of sales by type (close #32)

---
+++ src/modules/sale/components/SalesList.tsx
@@ -0,0 +1,38 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { SaleCard } from "./SaleCard";
+import { MegaphoneOff } from "lucide-react";
+
+interface SalesListProps {
+  sales: Sale[];
+  saleType: string;
+  isLoading: boolean;
+}
+
+export function SalesList({ sales, saleType, isLoading }: SalesListProps) {
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-8 mb-20">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Skeleton key={index} className="w-full h-44" />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-8 mb-20">
+      {sales && sales.length > 0 ? (
+        sales.map((sale) => (
+          <SaleCard key={sale.id} sale={sale} saleType={saleType} />
+        ))
+      ) : (
+        <div className="w-full flex flex-col gap-4 items-center justify-center mt-24">
+          <MegaphoneOff size={20} className="w-32 h-32 stroke-muted " />
+          <span className="text-lg">
+            No hay ventas de {saleType} disponibles en este momento.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/modules/sale/components/management/data.ts
@@ -0,0 +1,106 @@
+export const sales: Sale[] = [
+  {
+    id: 1,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 4,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 5,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 6,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 7,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+  {
+    id: 8,
+    date: "2021-08-25",
+    amount: 500,
+    items: [
+      {
+        id: 1,
+        name: "Producto 1",
+        quantity: 2,
+        price: 250,
+      },
+    ],
+  },
+];
