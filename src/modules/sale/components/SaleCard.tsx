2d40e4ff234f1aafdb64c2ba82f011abc4a863a2
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-04-23 13:52:58 -0500

    Implementing sales filter with date range picker

--- src/modules/sale/components/SaleCard.tsx
+++ src/modules/sale/components/SaleCard.tsx
@@ -1,23 +1,21 @@
 import { Card, CardHeader } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { ProgressDemo } from './ProgressDemo';
 import {Sale} from '@/types/sale';
 import { SaleDialog } from "./SaleDialog";
 
 interface SaleCardProps {
   sale: Sale;
-  saleType: string;
 }
 
-export function SaleCard({ sale, saleType }: SaleCardProps) {
+export function SaleCard({ sale}: SaleCardProps) {
   return (
-    <Card className={cn("flex rounded-xl overflow-hidden group hover:shadow-2xl bg-background ", saleType)}>
+    <Card className={cn("flex rounded-xl overflow-hidden group hover:shadow-2xl bg-background ")}>
       <div className="bg-background rounded-tr-lg rounded-br-lg shadow-lg p-6 flex-grow">
         <div className="flex justify-between items-center mb-4">
           <div>
-          <h3 className="text-xl font-bold">
-            Nombre del {sale.items[0]?.type === 'product' ? 'producto' : 'servicio'} {sale.id}
-          </h3>
+            <h3 className="text-xl font-bold">
+              Nombre del {sale.items[0]?.type === 'product' ? 'producto' : 'servicio'} {sale.id}
+            </h3>
             <p className="text-gray-500">{sale.sale_date}</p>
           </div>
           <div>
@@ -27,18 +25,16 @@ export function SaleCard({ sale, saleType }: SaleCardProps) {
         <div className="flex items-center justify-between">
           <div>
             <p className="text-gray-500">Cliente:</p>
-            <p className="font-bold">Jhon Doe</p>
+            <p className="font-bold">{sale.customer_id}</p>
           </div>
           <div>
             <p className="text-gray-500">Vendedor:</p>
-            <p className="font-bold">Unknown</p>
+            <p className="font-bold">{sale.user_id}</p> 
           </div>
         </div>
-        {sale.items[0]?.type === 'service' && <ProgressDemo />}{
-        }
       </div>
       <div className="flex justify-end">
-        <SaleDialog sale={sale} saleType={saleType}/>
+        <SaleDialog sale={sale}/>
       </div>
     </Card>
   );
--- src/modules/sale/components/SalesList.tsx
+++ src/modules/sale/components/SalesList.tsx
@@ -2,14 +2,26 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { SaleCard } from "./SaleCard";
 import { MegaphoneOff } from "lucide-react";
 import {Sale} from '@/types/sale';
+import { DateRange } from 'react-day-picker';
 
 interface SalesListProps {
   sales: Sale[];
-  saleType: string;
   isLoading: boolean;
+  dateRange?: DateRange;
 }
 
-export function SalesList({ sales, saleType,isLoading }: SalesListProps) {
+export function SalesList({ sales,isLoading, dateRange}: SalesListProps) {
+  const filteredSales = sales.filter(sale => {
+    if (!dateRange) {
+      return true;
+    }
+
+    const saleDate = new Date(sale.sale_date);
+    const { from, to } = dateRange;
+
+    return saleDate >= (from ?? new Date(0)) && (!to || saleDate <= to);
+  });
+
   if (isLoading) {
     return (
       <div className="flex flex-col gap-8 mb-20">
@@ -22,15 +34,15 @@ export function SalesList({ sales, saleType,isLoading }: SalesListProps) {
 
   return (
     <div className="flex flex-col gap-8 mb-20">
-      {sales && sales.length > 0 ? (
-        sales.map((sale) => (
-          <SaleCard key={sale.id} sale={sale} saleType={sale.items[0]?.type}/>
+      {filteredSales && filteredSales.length > 0 ? (
+        filteredSales.map(sale => (
+          <SaleCard key={sale.id} sale={sale} />
         ))
       ) : (
         <div className="w-full flex flex-col gap-4 items-center justify-center mt-24">
           <MegaphoneOff size={20} className="w-32 h-32 stroke-muted " />
           <span className="text-lg">
-            No hay ventas de {saleType} disponibles en este momento.
+            No hay ventas de disponibles en este momento.
           </span>
         </div>
       )}
