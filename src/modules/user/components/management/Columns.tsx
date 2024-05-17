2b4f4b1ac24ca6ae413cd931b8e15f3c311d08c2
arnold <camposarnoldcn@gmail.com>
2024-05-16 23:46:31 -0500

    Checkbox borrados de la tabla usuarios

--- src/modules/user/components/management/Columns.tsx
+++ src/modules/user/components/management/Columns.tsx
@@ -1,5 +1,4 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { Checkbox } from "@/components/ui/checkbox";
 import { User, User as UserDetailType } from "@/types/auth";
 import { Button } from "@/components/ui/button";
 import { ArrowUpDown } from "lucide-react";
@@ -10,30 +9,6 @@ import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export const columns: ColumnDef<UserDetailType>[] = [
-  
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        className="ml-4"
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
   {
     accessorKey: "image",
     header: ({ column }) => {
@@ -176,3 +151,147 @@ export const columns: ColumnDef<UserDetailType>[] = [
     },
   },
 ];
+
+
+// export const columns: ColumnDef<UserDetailType>[] = [
+//   {
+//     accessorKey: "image",
+//     header: ({ column }) => {
+//       return (
+//         <div className="flex items-center justify-center">
+//           <span className="mr-2 ml-2">Perfil</span>
+//         </div>
+//       );
+//     },
+//     cell: ({ row }) => (
+//       /*Si es que hay imagen se muestra la imagen*/
+//       <div className="flex items-center justify-center">
+//         {row.getValue("image")?.includes("/media/photos") ? (
+//           <Avatar className="w-16 h-16">
+//             <AvatarImage
+//               src={row.getValue("image")}
+//               alt="Imagen de perfil"
+//               className="object-cover rounded-full w-full h-full max-w-full max-h-full"
+//               style={{ objectFit: 'cover' }}
+//             />
+//           </Avatar>
+//         ) : (
+//           /*Si no hay imagen se muestra el avatar con las iniciales*/
+//           <Avatar className="mx-auto  rounded-full w-16 h-16 flex-initial object-cover">
+//             <AvatarFallback className="text-1xl flex items-center justify-center h-full">
+//               {typeof row.getValue("name") === 'string' ? (row.getValue("name") as string)[0] : ''}
+//               {typeof row.getValue("lastname") === 'string' && row.getValue("lastname") ? (row.getValue("lastname") as string)[0] : ''}
+//             </AvatarFallback>
+//           </Avatar>
+//         )}
+//       </div>
+//     ),
+//   },
+//   {
+//     accessorKey: "lastname",
+//     header: ({ column }) => {
+//       return (
+//         <Button
+//           variant="ghost"
+//           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+//         >
+//           Apellidos
+//           <ArrowUpDown className="ml-2 h-4 w-4" />
+//         </Button>
+//       );
+//     },
+//     cell: ({ row }) => <div>{row.getValue("lastname")}</div>,
+//   },
+//   {
+//     accessorKey: "name",
+//     header: ({ column }) => {
+//       return (
+//         <Button
+//           variant="ghost"
+//           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+//         >
+//           Nombres
+//           <ArrowUpDown className="ml-2 h-4 w-4" />
+//         </Button>
+//       );
+//     },
+//     cell: ({ row }) => <div>{row.getValue("name")}</div>,
+//   },
+//   {
+//     accessorKey: "document_number",
+//     header: ({ column }) => {
+//       return (
+//         <Button
+//           variant="ghost"
+//           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+//         >
+//           Dni
+//           <ArrowUpDown className="ml-2 h-4 w-4" />
+//         </Button>
+//       );
+//     },
+//     cell: ({ row }) => (
+//       <div className="lowercase">{row.getValue("document_number")}</div>
+//     ),
+//   },
+//   {
+//     accessorKey: "role",
+//     header: ({ column }) => {
+//       return (
+//         <Button
+//           variant="ghost"
+//           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+//         >
+//           Rol
+//           <ArrowUpDown className="ml-2 h-4 w-4" />
+//         </Button>
+//       );
+//     },
+//     cell: ({ row }) => {
+//       const formated = row.getValue("role") == 1 ? "administrador" : "empleado";
+//       return <div className="lowercase">{formated}</div>;
+//     },
+//   },
+//   {
+//     accessorKey: "estado",
+//     header: "Estado",
+//     cell: ({ row }) => {
+//       const { is_active } = row.original;
+//       return is_active ? (
+//         <Badge
+//           variant="outline"
+//           className="border-green-500 text-green-500 capitalize"
+//         >
+//           {row.getValue("estado") || "Activo"}
+//         </Badge>
+//       ) : (
+//         <Badge
+//           variant="outline"
+//           className="border-red-500 text-red-500 capitalize"
+//         >
+//           {row.getValue("estado") || "Inactivo"}
+//         </Badge>
+//       );
+//     },
+//   },
+//   {
+//     id: "actions",
+//     enableHiding: false,
+//     cell: ({ row }) => {
+//       const user = row.original;
+//       const [open, setIsOpen] = useState(false);
+//       if (user.role_auth === 1) {
+//         return (
+//           <Sheet open={open} onOpenChange={setIsOpen}>
+//             <SheetTrigger asChild>
+//               <Button variant="outline">Ver</Button>
+//             </SheetTrigger>
+//             <UserDetail user={user} open={open} setIsOpen={setIsOpen} />
+//           </Sheet>
+//         );
+//       } else {
+//         return null; // No mostrar la columna de acciones si role_auth no es 1
+//       }
+//     },
+//   },
+// ];
